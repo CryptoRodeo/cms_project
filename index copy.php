2cc503542b71f54892508c37ef4ea212d8485e16
CryptoRodeo <b.r.tech95@gmail.com>
2020-01-26 09:28:49 -0500

    added pagination and styling to cms

--- index copy.php
+++ index copy.php
@@ -12,52 +12,7 @@
             <div class="col-md-8">
 
             <?php
-
-            $limit_per_page = 4;
-            if(isset($_GET['page']))
-            {
-                $page_index = $_GET['page'];
-            }
-            else
-            {
-                $page_index="";
-            }
-
-            //if @ homepage
-            if($page_index == "" || $page_index == 1)
-            {
-                $offset = 0; //retrieve the first row
-            }
-            else
-            {
-                //offset is (current_page_index * 5) - 5
-                /**
-                 * This gives us the distance away from the beginning
-                 * 
-                 * example:
-                 * 
-                 * if page = 2
-                 * 
-                 * offset = (2 * 5) - 5 = 5
-                 * 
-                 * So now we're retrieving 5 values/pages from starting at index 5 
-                 */
-                $offset = ($page_index * $limit_per_page) - $limit_per_page; 
-            }
-                $post_query_count = "SELECT * FROM posts";
-                $find_count = mysqli_query($connection, $post_query_count);
-                $count = mysqli_num_rows($find_count);
-
-                //get the ceiling value of the amount of posts in the db / the limit set per page.
-                /**
-                 * So if theres 15 posts and the limit is 3 then the result will be 5
-                 * 
-                 * If theres 15 posts and the limit is 4 then the result will be 4 and etc.
-                 */
-                $count = ceil($count / $limit_per_page);
-
-
-                $query = "SELECT * FROM posts WHERE post_status = 'published' LIMIT $offset, $limit_per_page";
+                $query = "SELECT * FROM posts WHERE post_status = 'published' ";
 
                 $select_all_posts = mysqli_query($connection, $query);
 
@@ -84,7 +39,6 @@
                 </h1>
 
                 <!-- First Blog Post -->
-                <h1><?php echo $count; ?></h1>
                 <h2>
                     <a href="./post.php?p_id=<?php echo $post_id; ?>"><?php echo $title ?></a>
                 </h2>
@@ -113,31 +67,4 @@
         </div>
         <!-- /.row -->
     </div>
-
-
-    <ul class="pager">
-        <?php
-            for($i = 1; $i <= $count; $i++)
-            {
-                if($i == $page_index)
-                {
-                    echo 
-                    "
-                        <li>
-                            <a class='active_link' href='index.php?page={$i}'>{$i}</a>
-                        </li>
-                    ";
-                }
-                else
-                {
-                    echo 
-                    "
-                        <li>
-                            <a href='index.php?page={$i}'>{$i}</a>
-                        </li>
-                    ";
-                }
-            }
-        ?>
-    </ul>
 <?php include "./includes/footer.php";?>
